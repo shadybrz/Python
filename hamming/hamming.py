9c695cb0e7f675de57a055d4b422b6b7a536d5d0
shady <66484379+shadybrz@users.noreply.github.com>
2021-07-12 11:43:17 +0430

    Add files via upload

---
+++ hamming/hamming.py
@@ -0,0 +1,9 @@
+def distance(strand_a, strand_b):
+    count=0
+    if(len(strand_a) != len(strand_b)):
+        raise ValueError("ValueError")
+    else:
+        for i in range(len(strand_a)):
+            if strand_a[i] != strand_b[i]:
+                count+=1
+        return count
