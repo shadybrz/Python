40e2053fe95e4f4f48034f2b8c836195e7c6099b
shady <66484379+shadybrz@users.noreply.github.com>
2021-08-03 18:33:44 +0430

    Update tournament.py

--- tournament/tournament.py
+++ tournament/tournament.py
@@ -1,40 +1,40 @@
 def tally(rows):
-    match_score = {}
+    table = {}
     for row in rows:
-        team1,team2,Result = row.split(';')
-        if team1 not in match_score:
-            match_score[team1] = {'W': 0, 'L': 0, 'D': 0, 'P': 0, 'MP': 0}
-        if team2 not in match_score:
-            match_score[team2] = {'W': 0, 'L': 0, 'D': 0, 'P': 0, 'MP': 0}
-        team_1 = match_score.get(team1)
-        team_2 = match_score.get(team2)
+        first_team,second_team,result = row.split(';')
+        if first_team not in table:
+            table[first_team] = {'W': 0, 'L': 0, 'D': 0, 'P': 0, 'MP': 0}
+        if second_team not in table:
+            table[second_team] = {'W': 0, 'L': 0, 'D': 0, 'P': 0, 'MP': 0}
+        team1 = table.get(first_team)
+        team2 = table.get(second_team)
 
-        if Result == 'win':
-            team_1["W"]+=1
-            team_2["L"]+=1
-            team_1["P"]+=3
-            team_1["MP"]+=1
-            team_2["MP"]+=1  
+        if result == 'win':
+            team1["W"]+=1
+            team2["L"]+=1
+            team1["P"]+=3
+            team1["MP"]+=1
+            team2["MP"]+=1  
 
-        elif Result == 'loss':
-            team_2["W"]+=1
-            team_1["L"]+=1
-            team_2["P"]+=3
-            team_1["MP"]+=1
-            team_2["MP"]+=1
+        elif result == 'loss':
+            team2["W"]+=1
+            team1["L"]+=1
+            team2["P"]+=3
+            team1["MP"]+=1
+            team2["MP"]+=1
 
-        elif Result == 'draw':
-            team_1["D"]+=1
-            team_2["D"]+=1
-            team_1["P"]+=1
-            team_2["P"]+=1
-            team_2["MP"]+=1
-            team_1["MP"]+=1        
+        elif result == 'draw':
+            team1["D"]+=1
+            team2["D"]+=1
+            team1["P"]+=1
+            team2["P"]+=1
+            team2["MP"]+=1
+            team1["MP"]+=1        
 
-    sort_score=sorted(list(match_score.items()),key=lambda x:x[0])
+    sort_score=sorted(list(table.items()),key=lambda x:x[0])
     sort_score.sort(key=lambda x: x[1]['P'], reverse=True)
     result_table = ["Team                           | MP |  W |  D |  L |  P"]
 
     for key, value in sort_score:
-        result_table.append(f'{key:<31}| {value["MP"]:>2d} | {value["W"]:>2d} | {value["D"]:>2d} | {value["L"]:>2d} | {value["P"]:>2d}')
+        result_table.append(f'{key:<31}| {value["MP"]:>2} | {value["W"]:>2} | {value["D"]:>2} | {value["L"]:>2} | {value["P"]:>2}')
     return result_table
